dc7c0c2665a90261e428cadcef8b3860b52ace94
Dario Mercuri <dario.mercuri31@gmail.com>
2022-11-10 20:56:23 -0500

    Add project files.

---
+++ CrudAPI/Controllers/RecordController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordController : ControllerBase
+    {
+        private static List<Record> records = new List<Record>{
+                new Record {
+                    Id = 1,
+                    Content = "Foo",
+                    Content2 = "Bar"
+                },
+                new Record {
+                    Id = 2,
+                    Content = "Fizz",
+                    Content2 = "Buzz"
+                }
+            };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Record>>> Get()
+        {
+            
+            return Ok(records);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<Record>>> AddRecord(Record record)
+        {
+            records.Add(record);
+            return Ok(records);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Record>>> Get(int id)
+        {
+            var rec = records.Find(x => x.Id == id);
+            return Ok(rec);
+        }
+    }
+}
---
+++ CrudAPI/Record.cs
@@ -0,0 +1,9 @@
+﻿namespace CrudAPI
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public string Content2 { get; set; } = string.Empty;
+    }
+}
